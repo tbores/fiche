c3cceafa88ebcd394a300eabe96733c5afa041fc
Thomas Bores <thomas.bores@gmail.com>
2014-03-01 15:36:42 +0100

    Add save function that handle the export of csv and xlsx documents

--- fiche.py
+++ fiche.py
@@ -9,7 +9,7 @@ import xlsxwriter
 import csv
 
 # Constants
-VERSION = 'v1.2'
+VERSION = 'v1.3'
 
 # Global 
 global_args = None # Program's command line arguments
@@ -27,32 +27,49 @@ def md5sum(file_path, block_size=1024):
         print('IOerror: '+ file_path)
     return md5.hexdigest()
 
-def save_hashlist(filename, hashlist):
-    try:
-        f = open(filename, 'w', newline='')
-        writer = csv.writer(f, delimiter=',')
-        for line in hashlist:
-            writer.writerow(line)
-        f.close()
-    except IOError:
-        print('IOError: '+filename)
+def save(list_of_hashlists):
+    # Save as csv
+    save_as_csv(list_of_hashlists)
+
+    # Save as xlsx
+    if global_args.xlsx is not None:
+        save_as_excel(global_args.xlsx, list_of_hashlists)
+
+def save_as_csv(list_of_hashlists):
+    for name, hashlist in list_of_hashlists:
+        if hashlist is not None:
+            try:
+                f = open(name+'csv', 'w', newline='')
+                print('Writing results in '+name+'.csv')
+                writer = csv.writer(f, delimiter=',')
+                for line in hashlist:
+                    writer.writerow(line)
+                f.close()
+            except IOError:
+                print('IOError: '+filename)
 
 def save_as_excel(filename, list_of_hashlists):
+    print('Writing results in '+global_args.xlsx)
+    
     if not filename.endswith('.xlsx'):
         filename += '.xlsx'
 
     workbook = xlsxwriter.Workbook(filename)
 
     for name, hashlist in list_of_hashlists:
-        worksheet = workbook.add_worksheet(name)
-        for row, line in enumerate(hashlist):
-            for i in range(0, len(line)):
-                worksheet.write(row, i, line[i])
-                worksheet.write(row, i, line[i])
+        if hashlist is not None:
+            worksheet = workbook.add_worksheet(name)
+            for row, line in enumerate(hashlist):
+                for i in range(0, len(line)):
+                    worksheet.write(row, i, line[i])
+                    worksheet.write(row, i, line[i])
 
 def handle_directory(directory_path):
     files_hashes = list()
-    files_duplicates = list()
+    files_duplicates = None
+
+    if global_args.duplicates:
+        files_duplicates = list()
     if os.path.isdir(directory_path):
         for root, dirs, files in os.walk(directory_path):
             num_files = len(files)
@@ -73,11 +90,13 @@ def handle_file(files_hashes, files_duplicates, filepath):
     md5_hash = md5sum(filepath)
     hashes_list = (el[1] for el in files_hashes)
 
-    # Check for duplicated files
-    if md5_hash in hashes_list:
-        for f_path, f_hash in files_hashes:
-            if f_hash == md5_hash:
-                files_duplicates.append((filepath,md5_hash,f_path,f_hash))
+    if files_duplicates is not None:
+        # duplicates argument has been set
+        # Check for duplicated files
+        if md5_hash in hashes_list:
+            for f_path, f_hash in files_hashes:
+                if f_hash == md5_hash:
+                    files_duplicates.append((filepath,md5_hash,f_path,f_hash))
     
     files_hashes.append((filepath,md5_hash))
 
@@ -92,27 +111,26 @@ def cmp_hashlist(left_hashlist, right_hashlist):
     return left_only_items
 
 def main(left_directory, right_directory):
+    list_of_hashlist = list()
+
     # Handle left
     left_hashlist,left_duplicates = handle_directory(left_directory)
-    save_hashlist("left.csv", left_hashlist)
-    save_hashlist("left_duplicates.csv", left_duplicates)
+    list_of_hashlist.append(( 'left', left_hashlist))
+    list_of_hashlist.append(('left_duplicates', left_duplicates))
 
     # Handle right
     right_hashlist, right_duplicates = handle_directory(right_directory)
-    save_hashlist("right.csv", right_hashlist)
-    save_hashlist("right_duplicates.csv", right_duplicates)
+    list_of_hashlist.append(('right', right_hashlist))
+    list_of_hashlist.append(('right_duplicates', right_duplicates))
 
     # Compare
-    save_hashlist("left_only.csv", cmp_hashlist(left_hashlist, right_hashlist))
-    save_hashlist("right_only.csv", cmp_hashlist(right_hashlist, left_hashlist))
+    list_of_hashlist.append(('left_only', 
+                            cmp_hashlist(left_hashlist, right_hashlist)))
+    list_of_hashlist.append(('right_only',
+                            cmp_hashlist(right_hashlist, left_hashlist)))
     
-    if global_args.xlsx is not None:
-        print('Writing results in '+global_args.xlsx)
-        save_as_excel(global_args.xlsx,
-                        [('left', left_hashlist),
-                        ('left_duplicates', left_duplicates),
-                        ('right', right_hashlist),
-                        ('right_duplicates', right_duplicates)])
+    # Write results
+    save(list_of_hashlist)
 
 if __name__ == "__main__":
     
@@ -125,7 +143,13 @@ if __name__ == "__main__":
                         help='Right (target) directory',
                         action='store')
     parser.add_argument('--xlsx', nargs='?', help='Generate an Excel file that\
-                         contain the results')
+                         contains the results')
+    parser.add_argument('--duplicates', nargs='?', help='Find duplicated files\
+                         on both sides. Produce two lists: left_duplicates and\
+                         and right_duplicates')
+    parser.add_argument('--ignore', nargs='*', help='Regular expression that\
+                         contains the files that will be ignored during the\
+                         analyze')
     global_args = parser.parse_args()
     print('Welcome to fiche '+VERSION)
     print('Thanks for using it!')
